df8fb352218cffab8b3c24612050ca25458a9b02
conghucai <conghucai@163.com>
2021-10-19 16:53:18 +0800

    cai002

--- src/com/mycode/Student.java
+++ src/com/mycode/Student.java
@@ -3,8 +3,9 @@ package com.mycode;
 public class Student {
 
     String id;
+    String id2;
 
     public void print(){
-        System.out.println("Git 001");
+        System.out.println("Git 002");
     }
 }
